8600f7e84f139b660f19f1dc7b9696ff4e567594
Abdelhadi <abdelhadikhadir@gmail.com>
2024-04-19 13:42:14 +0200

    test1

---
+++ Object Repository/Page_Google/div_Tout accepter.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Tout accepter</name>
+   <tag></tag>
+   <elementGuidId>72fb01b8-5019-4f31-817b-d5a8d389b20c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#L2AGLb > div.QS5gu.sy4vM</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='L2AGLb']/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>b7567704-edaf-4ae8-992c-28e549725055</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>QS5gu sy4vM</value>
+      <webElementGuid>35d35752-e81c-4bf8-b752-09178353a720</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>none</value>
+      <webElementGuid>07c3bc53-c2d8-45f7-8c8b-6ff46e6f7fb2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Tout accepter</value>
+      <webElementGuid>f5befe31-a704-4f8d-a41b-25a94c70c5e6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;L2AGLb&quot;)/div[@class=&quot;QS5gu sy4vM&quot;]</value>
+      <webElementGuid>b0481542-df15-4e25-88db-9c9dc7016539</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//button[@id='L2AGLb']/div</value>
+      <webElementGuid>65b4aeca-27d4-4e0d-8516-6a690acc65ee</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Tout refuser'])[1]/following::div[1]</value>
+      <webElementGuid>f1d15064-3d8f-45bc-b130-2f8879fe49e7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='g.co/privacytools'])[1]/following::div[4]</value>
+      <webElementGuid>238510a0-b2cd-4bb5-89eb-8075a5668257</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)=concat('Plus d', &quot;'&quot;, 'options')])[1]/preceding::div[1]</value>
+      <webElementGuid>0d19d8f3-a2f8-467b-bdf1-f891e416faa0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Confidentialité'])[2]/preceding::div[2]</value>
+      <webElementGuid>a0f63cf2-8185-4655-b3cb-455788e94f8d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button[2]/div</value>
+      <webElementGuid>f79a3966-1dc3-405e-94ca-66a7b48aff2a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Tout accepter' or . = 'Tout accepter')]</value>
+      <webElementGuid>0df0c97e-34eb-4a57-9083-a927ae7d3fdf</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
